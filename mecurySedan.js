c285d42e8bc2dcc151c3644f4b3227ce4c67d140
unknown <ramiropen.work@gmail.com>
2022-02-03 16:49:46 -0800

    first commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,4 +1,126 @@
 //this includes the vehicle class as a module
+class car extends vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year,color, mileage); 
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.maxPassenger = 6;
+        this.passanger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
+        this.started = false;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    }
+
+    // had to get this from solution code, could not fiure it out
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+    // this also, i tried toc omplete before class
+    scheduleService(mileage) {
+        if (this.mileage > 30000) {            
+            this.scheduleService == true
+            return this.scheduleService;                       
+         }
+}
+
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
+    accelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                console.log(this.speed += 1);
+                this.fuel = this.fuel - 1;
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    decelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                if (this.speed > 0) {
+                    console.log(this.speed -= 1);
+                    this.fuel = this.fuel - 1;
+                } else {
+                    console.log(this + " has stopped moving");
+                    this.fuel = this.fuel - 1;
+                }
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    stop() {
+        this.started = false;
+    }
+
+    drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+
+    autoPark()
+    {
+
+    }
+
+    autoDrive()
+    {
+      
+    }
+
+    typeOfVehicle(wheels) {
+        if (this.numberOfWheels == 8 && 8 == wheels) {
+            console.log(this.model + " " + this.make + " is a Truck");
+        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+            console.log(this.model + " " + this.make + " is a CAr");
+        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+            console.log(this.model + " " + this.make + " is a Bike");
+        } else {
+            console.log("Unknown type of vehicle");
+        }
+    }
+}
+
+//This exports things you want to use from this "module", more info in readme
+module.exports = {
+    Vehicle
+}
+
+
 const VehicleModule = require("./vehicle")
 
 //this shows how to call from this module...
